30c832b7f69d4cced63c4c7e9c8ce5bdc9fd809b
JerryHaaser <jerry.haaser@hotmail.com>
2019-08-26 14:47:14 -0500

    Added 6 potential views and made LgInViewController file

---
+++ AnywhereFitness/AnywhereFitness/LogInViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LogInViewController.swift
+//  AnywhereFitness
+//
+//  Created by admin on 8/26/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func clientButton(_ sender: Any) {
+    }
+    @IBAction func trainerButton(_ sender: Any) {
+    }
+    @IBAction func logInButton(_ sender: Any) {
+    }
+    @IBAction func forgotPasswordButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
