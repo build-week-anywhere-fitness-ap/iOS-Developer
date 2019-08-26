23410df3a8ca61983e18fe07c9eda5113b3b52b7
William Chen <chen.will7@gmail.com>
2019-08-26 15:58:15 -0700

    Finished creating the files and matching the class with it

---
+++ AnywhereFitness/AnywhereFitness/ViewController/ClientForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ClientForgotPasswordViewController.swift
+//  AnywhereFitness
+//
+//  Created by William Chen on 8/26/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class ClientForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
