0db1d900d4a49a0aeedea22686b8179faac5520b
JerryHaaser <jerry.haaser@hotmail.com>
2019-08-26 21:20:23 -0500

    Added a few view controllers and created corresponding files

---
+++ AnywhereFitness/AnywhereFitness/ViewController/TrainerViewControllers/CreateCourseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateCourseViewController.swift
+//  AnywhereFitness
+//
+//  Created by admin on 8/26/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class CreateCourseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
