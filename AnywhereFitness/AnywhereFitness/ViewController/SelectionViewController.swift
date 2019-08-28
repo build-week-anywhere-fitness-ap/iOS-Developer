db6208551b7a3047d77b264b07b58a5c6bd3b0db
JerryHaaser <jerry.haaser@hotmail.com>
2019-08-28 11:06:24 -0500

    added two buttons

--- AnywhereFitness/AnywhereFitness/ViewController/SelectionViewController.swift
+++ AnywhereFitness/AnywhereFitness/ViewController/SelectionViewController.swift
@@ -16,6 +16,13 @@ class SelectionViewController: UIViewController {
         self.view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
     }
     
+    @IBAction func clientButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func trainerButtonTapped(_ sender: UIButton) {
+    }
+    
+    
 
     /*
     // MARK: - Navigation
